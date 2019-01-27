6cee0bc57c64d7d7548395e02c3dc640362f5b08
tehblasian <jeremiahdavid.wreh@gmail.com>
2019-01-26 23:17:08 -0500

    Added bare structure for frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+
+import Dashboard from './pages/Dashboard';
+
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/join" component={Dashboard}/>
+                </Switch>
+            </BrowserRouter>
+        );
+    }
 }
-
+    
 export default App;
+    
